df1c128e2d39f8f296c4e8b7938f2b110222dd67
Malganis66 <isiprocrack@gmail.com>
2022-12-05 20:08:54 -0300

    enemyx3

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById("canvas1")
 const ctx = canvas.getContext("2d")
 CANVAS_WIDTH = canvas.width = 500
 CANVAS_HEIGHT = canvas.height = 1000
-const numberOfEnemies = 20
+const numberOfEnemies = 35
 const enemiesArray = []
 
 let gameFrame = 0
@@ -11,10 +11,10 @@ let gameFrame = 0
 class Enemy {
     constructor(){
         this.image = new Image()
-        this.image.src = 'enemy2.png'
+        this.image.src = 'enemy3.png'
         this.speed = Math.random() * 4 + 1
-        this.spriteWidth = 266
-        this.spriteHeight = 188
+        this.spriteWidth = 218
+        this.spriteHeight = 177
         this.width = this.spriteWidth/2;
         this.height = this.spriteHeight/2;
         this.x = Math.random() * (canvas.width - this.width);
@@ -22,12 +22,12 @@ class Enemy {
         this.frame = 0
         this.flapSpeed = Math.floor(Math.random() * 3 + 1)
         this.angle = 0
-        this.angleSpeed = Math.random() * 0.2
-        this.curve = Math.random() * 10
+        this.angleSpeed = Math.random() * 1.5 + 0.5
+        // this.curve = Math.random() * 200 + 50
     }
     update(){
-        this.x-= this.speed;
-        this.y+= this.curve * Math.sin(this.angle);
+        this.x = canvas.width/2 * Math.cos(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2);
+        this.y = canvas.height/2 * Math.sin(this.angle * Math.PI/270) + (canvas.height/2 - this.height/2);
         this.angle+=this.angleSpeed
         if(this.x + this.width < 0) this.x = canvas.width
         // animateSprite
